3196f6a1afc7483726590fc3a5843f43942bd200
asu-suzuki <lynch.232016@gmail.com>
2020-11-03 20:54:37 +0900

    Remove: 不要コード削除

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,30 +2,6 @@ import React from "react";
 import "./css/Form.css";
 
 const Form = (props, handleNewTask, handleSubmit) => {
-  // // const [task, setTask] = useState("");
-
-  // const handleNewTask = (e) => {
-  //   props.setTask(e.target.value);
-  // };
-
-  // const handleSubmit = (e) => {
-  //   e.preventDefault();
-  //   if (props.task === "") {
-  //     return;
-  //   }
-
-  //   const newTodo = {
-  //     task: props.task,
-  //     completed: false,
-  //   };
-
-  //   // props.setTodo((todos) => [...todos, { props.task, completed: false }]);
-  //   // setTodos
-  //   props.setTodo([...props.todos, newTodo]);
-
-  //   props.setTask("");
-  // };
-
   return (
     <form className="todos-header" onSubmit={handleSubmit}>
       <input
